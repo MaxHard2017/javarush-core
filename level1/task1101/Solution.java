1e2436335e5cb85be9a2514e8cbcaf747a496cc7
Max IV <maxhard001@gmail.com>
2022-09-12 18:52:10 +0300

    Level11 - примеры по наследованию

---
+++ level1/task1101/Solution.java
@@ -0,0 +1,24 @@
+package level1.task1101;
+
+/*
+ * Отредактируй два класса — Horse (лошадь) и Pegasus (пегас).
+    Унаследуй пегаса от лошади.
+    Требования:
+        •        В классе Solution должен быть класс Horse (лошадь).
+        •        Класс Horse должен быть public.
+        •        В классе Solution должен быть класс Pegasus (пегас).
+        •        Класс Pegasus должен быть public.
+        •        Класс Pegasus должен быть унаследован от класса Horse.
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus extends Horse {
+
+    }
+}
