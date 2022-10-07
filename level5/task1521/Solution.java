5a4ee5cd9c54393c8a9ed4437f81e9965401f46b
Max IV <maxhard001@gmail.com>
2022-10-07 19:39:32 +0300

    Level 15 - перегрузка методов с разными типами данных

---
+++ level5/task1521/Solution.java
@@ -0,0 +1,41 @@
+package level5.task1521;
+/*
+ *  Два раза перегрузи в классе Tree метод info(Object s) так, чтобы получилось три метода: info(Object s), info(Number s), info(String s).
+    Разберись в методе info(Object s). Сделай по аналогии функционал новых методов.
+    Например, для метода info(Number s) результат может быть таким: "Дерево № 123 , метод Number, параметр Short".
+Требования:
+    •    В классе Tree должен быть реализован метод info(Object s).
+    •    В классе Tree должен быть реализован метод info(Number s).
+    •    В классе Tree должен быть реализован метод info(String s).
+    •    Метод info(Number s) должен выводить на экран строку, аналогичную строке из метода info(Object s), заменив лишь фразу("метод Object" на "метод Number").
+    •    Метод info(String s) должен выводить на экран строку, аналогичную строке из метода info(Object s), заменив лишь фразу("метод Object" на "метод String").
+ */
+
+import java.math.BigDecimal;
+
+/* 
+ООП. Перегрузка
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //Блок 2.
+        //Вызов для Object
+        new Tree().info((Object) Integer.valueOf("4"));
+        new Tree().info((Object) Short.valueOf("4"));
+        new Tree().info((Object) new BigDecimal("4"));
+
+        //Блок 3.
+        //Вызов для Number
+        new Tree().info(Integer.valueOf("4"));
+        new Tree().info(Short.valueOf("4"));
+        new Tree().info(new BigDecimal("4"));
+
+        //Блок 4.
+        //Вызов для String
+        new Tree().info(new String("4"));
+        new Tree().info(Integer.valueOf("4").toString());
+        new Tree().info(Short.valueOf("4").toString());
+        new Tree().info(new BigDecimal("4").toString());
+    }
+}
