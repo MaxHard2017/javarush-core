7fa55e4c8580076a818abb0e10f30e223ffe942a
Max IV <maxhard001@gmail.com>
2022-10-07 19:42:28 +0300

    Level 15 - херня

---
+++ level5/task1523/SubSolution.java
@@ -0,0 +1,16 @@
+package level5.task1523;
+
+public class SubSolution extends Solution {
+
+    public SubSolution() {
+    }
+
+    protected SubSolution(int i) {
+        super(i);
+    }
+
+    SubSolution(String s) {
+        super(s);
+    }
+    
+}
