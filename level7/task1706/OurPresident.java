405a3559ab32f7beb5fd32239d29c4cd6862fb05
Max IV <maxhard001@gmail.com>
2022-10-19 01:21:55 +0300

    1706 Singleton инициализация в статическом блоке

---
+++ level7/task1706/OurPresident.java
@@ -0,0 +1,18 @@
+package level7.task1706;
+
+public class OurPresident {
+    private static OurPresident president;
+    static {
+        synchronized (OurPresident.class) {
+            OurPresident.president = new OurPresident();
+        }
+
+    }
+
+    private OurPresident() {
+    }
+
+    public static OurPresident getOurPresident() {
+        return president;
+    }
+}
