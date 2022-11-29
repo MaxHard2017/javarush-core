3eb3536b7758ee381e49fbbd5ebe12debf7948d1
Max <maxhard001@gmail.com>
2022-11-29 15:47:57 +0300

    2005 - запись состояния объекта в файл. Исправление ошибки

---
+++ level10/task2005/Asset.java
@@ -0,0 +1,50 @@
+package level10.task2005;
+
+public class Asset {
+    public Asset(String name) {
+        this.name = name;
+    }
+
+    private String name;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Asset other = (Asset) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+   
+    
+}
